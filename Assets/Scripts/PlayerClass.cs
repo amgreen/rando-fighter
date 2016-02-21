5a7b1e10d2ad82abc14a4765a69451d89d3da736
Samuel Van Cise <vancisesam@gmail.com>
2016-02-21 18:52:31 -0500

    Added 3 levels of punches

---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerClass : MonoBehaviour {
+
+    public float health = 100;
+    public Image myHealthBar;
+    private float initialHealth;
+
+    private enum AttackType {High, Middle, Low, Projectile};
+
+    public float attackTimeHigh = 1.0f;
+    public float attackTimeMiddle = 1.0f;
+    public float attackTimeLow = 1.0f;
+    public float attackTimeProjectile = 1.0f;
+    
+    public float attackDamageHigh = 10.0f;
+    public float attackDamageMiddle = 10.0f;
+    public float attackDamageLow = 10.0f;
+    public float attackDamageProjectile = 10.0f;
+
+    public bool currentlyAttacking = false;
+    public bool attackedAlready = false;
+
+    public GameObject myFist;
+    public GameObject middleFist;
+    public GameObject myFoot;
+
+    // Use this for initialization
+    void Start () {
+        initialHealth = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.U))
+        {
+            StartCoroutine(Attack(AttackType.High));
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            StartCoroutine(Attack(AttackType.Middle));
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            StartCoroutine(Attack(AttackType.Low));
+        }
+        myHealthBar.fillAmount = health / initialHealth;
+	}
+
+    IEnumerator Attack(AttackType executeAttack)
+    {
+        currentlyAttacking = true;
+        Debug.Log(executeAttack);
+        if (executeAttack == AttackType.High)
+        {
+            myFist.SetActive(true);
+            yield return new WaitForSeconds(attackTimeHigh);
+            myFist.SetActive(false);
+        }
+        if (executeAttack == AttackType.Middle)
+        {
+            middleFist.SetActive(true);
+            yield return new WaitForSeconds(attackTimeMiddle);
+            middleFist.SetActive(false);
+        }
+        if (executeAttack == AttackType.Low)
+        {
+            myFoot.SetActive(true);
+            yield return new WaitForSeconds(attackTimeLow);
+            myFoot.SetActive(false);
+        }      
+        currentlyAttacking = false;
+        attackedAlready = false;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject otherObject = collision.collider.gameObject;
+        if (otherObject.tag == "high")
+        {
+            if (otherObject.transform.parent.GetComponent<PlayerClass>().currentlyAttacking && !attackedAlready)
+            {
+                attackedAlready = true;
+                health -= attackDamageHigh;
+                Debug.Log("Health: " + health);
+            }
+        }
+        if (otherObject.tag == "middle")
+        {
+            attackedAlready = true;
+            health -= attackDamageMiddle;
+            Debug.Log("Health: " + health);
+        }
+        if (otherObject.tag == "low")
+        {
+            attackedAlready = true;
+            health -= attackDamageLow;
+            Debug.Log("Health: " + health);
+        }
+
+    }
+}
